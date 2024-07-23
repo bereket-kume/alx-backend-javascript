d04297724c5e5930b04263a219018e5564fbb06c
bereket-kume <bereketkume@gmail.com>
2024-07-23 15:34:15 +0300

    don

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -8,8 +8,9 @@ export function taskFirst() {
   }
   
   export function taskNext() {
-    const combination = 'But sometimes let';
+    let combination = 'But sometimes let';
     combination += getLast();
   
     return combination;
   }
+  
