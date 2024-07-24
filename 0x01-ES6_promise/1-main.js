ef3595b76d2a41bbd7e4cf6b38ec072ef7459547
bereket-kume <bereketkume@gmail.com>
2024-07-24 15:44:44 +0300

    done

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,3 +1,23 @@
-import handleProfileSignup from './6-final-user.js';
+import loadBalancer from './7-load_balancer.js';
 
-console.log(handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg'));
+const ukSuccess = 'Downloading from UK is faster';
+const frSuccess = 'Downloading from FR is faster';
+
+const promiseUK = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, ukSuccess);
+});
+
+const promiseUKSlow = new Promise((resolve, reject) => {
+  setTimeout(resolve, 400, ukSuccess);
+});
+
+const promiseFR = new Promise((resolve, reject) => {
+  setTimeout(resolve, 200, frSuccess);
+});
+
+const test = async () => {
+  console.log(await loadBalancer(promiseUK, promiseFR));
+  console.log(await loadBalancer(promiseUKSlow, promiseFR));
+};
+
+test();
