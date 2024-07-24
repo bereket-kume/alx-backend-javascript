99208ccb9ffc01fc8a4553c1b121c644fb01b29d
bereket-kume <bereketkume@gmail.com>
2024-07-24 14:32:21 +0300

    done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { createUser, uploadPhoto } from './utils.js';
+import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
