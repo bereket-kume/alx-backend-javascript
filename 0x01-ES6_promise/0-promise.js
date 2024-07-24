60b5dbeef3bb264bc261d4ebe0228551e7ea7c13
bereket-kume <bereketkume@gmail.com>
2024-07-24 11:53:40 +0300

    lint fixed

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
-function getResponseFromAPI() {
-  return new Promise(() => {});
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Success');
+    }, 1000);
+  });
 }
