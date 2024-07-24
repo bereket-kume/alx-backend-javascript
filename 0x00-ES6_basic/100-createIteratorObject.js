5aa875c9c382d6ab76a54910c9769afdedaa99fb
bereket-kume <bereketkume@gmail.com>
2024-07-24 23:09:43 +0300

    done

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,9 @@
-export default function iterateThroughObject(reportWithIterator) {
-  console.log(reportWithIterator);
+export default function createIteratorObject(report) {
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
 }
