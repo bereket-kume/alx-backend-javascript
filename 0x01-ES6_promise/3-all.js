3c98a926fc48c721a958cb492ede572413f825d4
bereket-kume <bereketkume@gmail.com>
2024-07-24 14:35:33 +0300

    done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { createUser, uploadPhoto } from './utils';
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((res) => {
-      console.log(res[0].body, res[1].firstName, res[1].lastName);
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
