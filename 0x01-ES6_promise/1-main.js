cbf487fa2ac1a006b03fd8756cf600e6ed2806ae
bereket-kume <bereketkume@gmail.com>
2024-07-24 16:03:56 +0300

    done

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,4 +1,5 @@
+import guardrail from './9-try.js';
 import divideFunction from './8-try.js';
 
-console.log(divideFunction(10, 2));
-console.log(divideFunction(10, 0));
+console.log(guardrail(() => divideFunction(10, 2)));
+console.log(guardrail(() => divideFunction(10, 0)));
