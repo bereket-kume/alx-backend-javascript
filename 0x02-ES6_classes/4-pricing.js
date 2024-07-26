d5be7bd4121ec8d5d4e8ff6fe0cc42161774d61c
bereket-kume <bereketkume@gmail.com>
2024-07-26 15:17:36 +0300

    done

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,23 +2,26 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
-  get Name() {
+  get amount() {
     return this._amount;
   }
 
-  set Name(amount) {
+  set amount(amount) {
+    if ((typeof amount !== 'number') && !(amount instanceof Number)) {
+      throw new TypeError('Amount must be a number');
+    }
     this._amount = amount;
   }
 
-  get Currency() {
+  get currency() {
     return this._currency;
   }
 
-  set Currency(currency) {
+  set currency(currency) {
     if (!(currency instanceof Currency)) {
       throw new TypeError('Currency must be an instanceof of Currency');
     }
@@ -26,10 +29,16 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} ${this._currency.code}`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
     return amount * conversionRate;
   }
 }
