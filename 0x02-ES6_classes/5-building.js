2fd270d835d66a394a006bf1b9b7651d0c93a91b
bereket-kume <bereketkume@gmail.com>
2024-07-26 15:47:10 +0300

    done

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,17 @@
 export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
+    constructor(sqft) {
+      if (this.constructor === Building) {
+        throw new Error("Building is an abstract class and cannot be instantiated directly");
+      }
+      this._sqft = sqft;
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
+  
+    evacuationWarningMessage() {
+      throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
   }
-
-  get sqft() {
-    return this._sqft;
-  }
-}
+  
