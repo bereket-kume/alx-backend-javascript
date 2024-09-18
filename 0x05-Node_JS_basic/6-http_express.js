b9da473cbf9669a8c758e0654694cada092dbf49
bereket-kume <bereketkume@gmail.com>
2024-09-18 22:04:26 +0300

    http server

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School');
+  res.send('Hello Holberton School!');
 });
 
 const port = 1245;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+module.exports = app;
