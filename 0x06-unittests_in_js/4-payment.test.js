6246b327aafb7600176864dda383a045c4255c81
bereket-kume <bereketkume@gmail.com>
2024-09-21 00:48:39 +0300

    fix error

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,17 +1,20 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
-const assert = require('assert')
+const assert = require('assert');
 
 describe('sendPaymentRequestToApi', () => {
     it('should call utils.calculateNumber with correct arguments', () => {
-        const myStub = sinon.stub(Utils, 'calculateNumber').returns(10)
-        const spy = sinon.spy(console, "log")
+        const myStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        const spy = sinon.spy(console, "log");
+        
         sendPaymentRequestToApi(100, 20);
+        
         assert(myStub.calledOnce);
-
-        assert(spy.withArgs("The total is: 10").calledOnce);
+        assert(spy.calledWith("The total is: 10"));
         assert(myStub.calledWith('SUM', 100, 20));
+        
         myStub.restore();
+        spy.restore();
     });
 });
