4b67ea49e2bef100419fa7ec861a63885542b207
bereket-kume <bereketkume@gmail.com>
2024-09-20 21:43:51 +0300

    basic test with mocha and node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul')
+
+
+describe('calculateNumber', function() {
+    it('should return 4 when inputs are 1 and 3', function() {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should return 5 when inputs are 1 and 3.7',() => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('should return 5 when inputs are 1.2 and 3.7', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should return 6 when inputs are 1.5 and 3.7', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+
+    it('should handle negative numers correctly', () => {
+        assert.strictEqual(calculateNumber(-1.2, 3.7), 3);
+    });
+
+    it('should return 0 when both inputs are 0.4', () => {
+        assert.strictEqual(calculateNumber(0.4, 0.4), 0);
+    });
+
+    it('should return -2 when oth inputs are -1.4 and -0.7', () => {
+        assert.strictEqual(calculateNumber(-1.4, -0.7), -2);
+    });
+})
