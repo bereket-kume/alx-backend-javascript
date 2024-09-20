62e7df6007b484c33228bc8ea4d1041c36474169
bereket-kume <bereketkume@gmail.com>
2024-09-21 00:41:15 +0300

    fix error

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,20 +1,29 @@
 const sinon = require('sinon');
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
-const assert = require('assert');
 const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-    it('should call utils.calculateNumber with correct arguments', () => {
-        const myStub = sinon.stub(Utils, 'calculateNumber')
-        myStub.returns(10)
-        sendPaymentRequestToApi(100, 20);
-        assert(myStub.calledOnce);
-        assert(myStub.calledWith('SUM', 100, 20));
-        myStub.restore();
+    let stub;
+    let spy;
+
+    beforeEach(() => {
+        stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        spy = sinon.spy(console, 'log');
     });
+
+    afterEach(() => {
+        stub.restore();
+        spy.restore();
+    });
+
+    it('should call Utils.calculateNumber with correct arguments', () => {
+        sendPaymentRequestToApi(100, 20);
+        expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    });
+
     it('should log the correct message', () => {
         sendPaymentRequestToApi(100, 20);
-        expect(myStub.calledWith('The total is: 10')).to.be.true;
+        expect(spy.calledWith('The total is: 10')).to.be.true;
     });
 });
