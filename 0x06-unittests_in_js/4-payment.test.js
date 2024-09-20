4a32733b505bda60eb5310aeab67fc09c14c8196
bereket-kume <bereketkume@gmail.com>
2024-09-21 00:39:02 +0300

    stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,7 +1,8 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
-const assert = require('assert')
+const assert = require('assert');
+const { expect } = require('chai');
 
 describe('sendPaymentRequestToApi', () => {
     it('should call utils.calculateNumber with correct arguments', () => {
@@ -11,5 +12,9 @@ describe('sendPaymentRequestToApi', () => {
         assert(myStub.calledOnce);
         assert(myStub.calledWith('SUM', 100, 20));
         myStub.restore();
+    });
+    it('should log the correct message', () => {
+        sendPaymentRequestToApi(100, 20);
+        expect(myStub.calledWith('The total is: 10')).to.be.true;
     });
 });
