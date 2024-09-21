bfec58d10b897b531ca7f075f4203905084036e3
bereket-kume <bereketkume@gmail.com>
2024-09-21 21:55:13 +0300

    fix error

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,9 +1,9 @@
-const getaymentTokenFromAPI = require("./6-payment_token");
-const expect = require("chai")
+const getPaymentTokenFromAPI = require("./6-payment_token");
+const { expect } = require("chai")
 
 describe("getPaymentTokenFromApi", () => {
     it("async testing with done callback", (done) => {
-        getaymentTokenFromAPI(true)
+        getPaymentTokenFromAPI(true)
           .then((data) => {
             expect(data).to.have.property('data');
             done();
